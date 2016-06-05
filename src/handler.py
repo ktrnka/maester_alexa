02af44787210ba1698f54355f5eba8899bca902c
Keith Trnka <keith.trnka@gmail.com>
2016-06-05 12:33:33 -0700

    Initial files

---
+++ src/handler.py
@@ -0,0 +1,287 @@
+"""
+This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
+as testing instructions are located at http://amzn.to/1LzFrj6
+
+For additional samples, visit the Alexa Skills Kit Getting Started guide at
+http://amzn.to/1LGWsLG
+"""
+
+from __future__ import print_function
+
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "GetCharacterInfo":
+        return get_character_info(intent, session)
+    elif intent_name == "GetHouseWords":
+        return get_house_words(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Welcome to Ask a Maester. Try asking who is Jon Snow."
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Ask about a character such as, " \
+                    "who is Jon Snow."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+                    "Have a nice day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
+_CHAR_INFO = {
+    "Catelyn Stark": "Lady Catelyn Stark also called Catelyn Tully is the wife of Lord Eddard Stark and Lady of Winterfell. Together they have five children: Robb, Sansa, Arya, Bran, and Rickon. Catelyn was born into House Tully of Riverrun, the liege lords of the riverlands. She is the daughter of Lord Hoster Tully and Lady Minisa Whent, and her siblings are Lysa and Edmure.",
+    "Jon Snow": "Jon Snow is the bastard son of Eddard Stark, by a mother whose identity is a source of speculation. He was raised by his father alongside his true-born half-siblings, but joins the Night's Watch when he nears adulthood. He is constantly accompanied by his albino direwolf Ghost. At the beginning of A Game of Thrones, Jon is fourteen years old. He is one of the major POV characters in the books. In the television adaptation Game of Thrones, Jon is portrayed by Kit Harington.",
+    "Eddard Stark": "Eddard Stark, also called Ned, is the head of House Stark, Lord of Winterfell, and Warden of the North. He is a close friend to King Robert I Baratheon, with whom he was raised. Eddard is one of the major POV characters in the books. In the television adaptation Game of Thrones, he is played by Sean Bean in Season 1 and by Sebastian Croft and Robert Aramayo during flashbacks in Season 6.",
+    "Daenerys Targaryen": "Princess Daenerys Targaryen, known as Daenerys Stormborn and Dany, is one of the last confirmed members of House Targaryen, along with her older brother Viserys, and she is one of the major POV characters in A Song of Ice and Fire. In the television adaptation Game of Thrones, Daenerys is played by Emilia Clarke.",
+    "Tyrion Lannister": "Tyrion Lannister is a member of House Lannister and is the third and youngest child of Lord Tywin Lannister and the late Joanna Lannister. His older siblings are Cersei Lannister, the queen of King Robert Baratheon, and Ser Jaime Lannister, a knight of Robert's Kingsguard. Tyrion is a dwarf; because of this he is sometimes mockingly called the Imp and the Halfman. He is one of the major POV characters in the books. In the television adaptation Game of Thrones, Tyrion is played by Peter Dinklage.",
+    "Arya Stark": "Arya Stark is the third child and second daughter of Lord Eddard Stark and Lady Catelyn Tully. A member of House Stark, she has five siblings: brothers Robb, Bran, Rickon, half-brother Jon Snow, and older sister Sansa. She is a POV character in A Song of Ice and Fire and is portrayed by Maisie Williams in the television adaptation, Game of Thrones. Like some of her siblings, Arya sometimes dreams that she is a direwolf. Her own direwolf is Nymeria, who is named in reference to the Rhoynar warrior-queen of the same name."
+}
+
+CHAR_INFO = {k.lower(): v for k, v in _CHAR_INFO.items()}
+
+
+def get_character_info(intent, session):
+    """Get information about a character"""
+
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = False
+
+    if 'character' in intent['slots']:
+        lookup = intent["slots"]["character"]["value"].lower()
+        card_title = lookup.title()
+        if lookup in CHAR_INFO:
+            speech_output = CHAR_INFO[lookup]
+            reprompt_text = CHAR_INFO[lookup]
+        else:
+            speech_output = "I don't recognize that name. " \
+                            "Please try again."
+            reprompt_text = "I don't recognize that name. " \
+                            "You can ask by saying, " \
+                            "who is Jon Snow?"
+    else:
+        speech_output = "I'm not sure who you meant. " \
+                        "Please try again."
+        reprompt_text = "I'm not sure who you meant. " \
+                        "You can ask by saying, " \
+                        "who is Jon Snow?"
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+HOUSE_WORDS = {u'allyrion': u'No Foe May Pass',
+               u'ambrose': u'Never Resting',
+               u'arryn': u'As High as Honor',
+               u'ashford': u'Our Sun Shines Bright',
+               u'baratheon': u'Ours is the Fury',
+               u'beesbury': u'Beware Our Sting',
+               u'bolton': u'Our Blades Are Sharp',
+               u'buckwell': u'Pride and Purpose',
+               u'bulwer': u'Death Before Disgrace',
+               u'caron': u'No Song So Sweet',
+               u'cerwyn': u'Honed and Ready',
+               u'codd': u'Though All Men Do Despise Us',
+               u'crakehall': u'None so Fierce',
+               u'egen': u'By Day or Night',
+               u"flint of widow's watch": u'Ever Vigilant',
+               u'follard': u'None so Wise',
+               u'footly': u'Tread Lightly Here',
+               u'fossoway of cider hall': u'A Taste of Glory',
+               u'fowler': u'Let Me Soar',
+               u'graceford': u'Work Her Will',
+               u'grandison': u'Rouse Me Not',
+               u'greyjoy': u'We Do Not Sow',
+               u'hastwyck': u'None So Dutiful',
+               u'hightower': u'We Light the Way',
+               u'hornwood': u'Righteous in Wrath',
+               u'jordayne': u'Let It Be Written',
+               u'karstark': u'The Sun of Winter',
+               u'lannister': u'Hear Me Roar! Their unofficial motto, just as well known, states, "A Lannister always pays his debts."',
+               u'lonmouth': u'The Choice Is Yours',
+               u'mallister': u'Above the Rest',
+               u'marbrand': u'Burning Bright',
+               u'martell': u'Unbowed, Unbent, Unbroken',
+               u'merryweather': u'Behold Our Bounty',
+               u'mooton': u'Wisdom and Strength',
+               u'mormont': u'Here We Stand',
+               u'oakheart': u'Our Roots Go Deep',
+               u'peckledon': u'Unflinching',
+               u'penrose': u'Set Down Our Deeds',
+               u'piper': u'Brave and Beautiful',
+               u'plumm': u'Come Try Me',
+               u'redfort': u'As Strong as Stone',
+               u'royce': u'We Remember',
+               u'sarsfield': u'True to the Mark',
+               u'serrett': u'I Have No Rival',
+               u'smallwood': u'From These Beginnings',
+               u'stark': u'Winter is Coming',
+               u'stokeworth': u'Proud to Be Faithful',
+               u'swyft': u'Awake! Awake!',
+               u'swygert': u'Truth Conquers',
+               u'tallhart': u'Proud and Free',
+               u'targaryen': u'Fire and Blood',
+               u'tarly': u'First in Battle',
+               u'tollett': u'When All is Darkest',
+               u'toyne': u'Fly High, Fly Far',
+               u'trant': u'So End Our Foes',
+               u'tully': u'Family, Duty, Honor',
+               u'tyrell': u'Growing Strong',
+               u'velaryon': u'The Old, the True, the Brave',
+               u'waxley': u'Light in Darkness',
+               u'wendwater': u'For All Seasons',
+               u'wensington': u'Sound the Charge',
+               u'westerling': u'Honor, not Honors',
+               u'wode': u'Touch Me Not',
+               u'wydman': u'Right Conquers Might',
+               u'yronwood': u'We Guard the Way'}
+
+
+def get_house_words(intent, session):
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = False
+
+    if 'house' in intent['slots']:
+        lookup = intent["slots"]["house"]["value"].lower()
+        card_title = lookup.title()
+        if lookup in HOUSE_WORDS:
+            speech_output = HOUSE_WORDS[lookup]
+            reprompt_text = HOUSE_WORDS[lookup]
+        else:
+            speech_output = "I don't recognize that house. " \
+                            "Please try again."
+            reprompt_text = "I don't recognize that house. " \
+                            "You can ask by saying, " \
+                            "what are the words of house stark."
+    else:
+        speech_output = "I'm not sure what you meant. " \
+                        "Please try again."
+        reprompt_text = "I'm not sure what you meant. " \
+                        "You can ask by saying, " \
+                        "what are the words of house stark."
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
---
+++ src/make_houses.py
@@ -0,0 +1,98 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import pprint
+import sys
+import argparse
+
+words_raw = """House Allyrion - No Foe May Pass
+House Ambrose - Never Resting
+House Arryn - As High as Honor
+House Ashford - Our Sun Shines Bright
+House Baratheon - Ours is the Fury
+House Beesbury - Beware Our Sting
+House Bolton - Our Blades Are Sharp
+House Buckwell - Pride and Purpose
+House Bulwer - Death Before Disgrace
+House Caron - No Song So Sweet
+House Cerwyn - Honed and Ready
+House Codd - Though All Men Do Despise Us
+House Crakehall - None so Fierce
+House Egen - By Day or Night
+House Flint of Widow's Watch - Ever Vigilant
+House Follard - None so Wise
+House Footly - Tread Lightly Here
+House Fossoway of Cider Hall - A Taste of Glory
+House Fowler - Let Me Soar
+House Graceford - Work Her Will
+House Grandison - Rouse Me Not
+House Greyjoy - We Do Not Sow
+House Hastwyck - None So Dutiful
+House Hightower - We Light the Way
+House Hornwood - Righteous in Wrath
+House Jordayne - Let It Be Written
+House Karstark - The Sun of Winter
+House Lannister - Hear Me Roar! Their unofficial motto, just as well known, states, "A Lannister always pays his debts."
+House Lonmouth - The Choice Is Yours
+House Marbrand - Burning Bright
+House Martell - Unbowed, Unbent, Unbroken
+House Mormont - Here We Stand
+House Mallister - Above the Rest
+House Merryweather - Behold Our Bounty
+House Mooton - Wisdom and Strength
+House Oakheart - Our Roots Go Deep
+House Peckledon - Unflinching
+House Penrose - Set Down Our Deeds
+House Piper - Brave and Beautiful
+House Plumm - Come Try Me
+House Redfort - As Strong as Stone
+House Royce - We Remember
+House Sarsfield - True to the Mark
+House Serrett - I Have No Rival
+House Smallwood - From These Beginnings
+House Stark - Winter is Coming
+House Stokeworth - Proud to Be Faithful
+House Swygert - Truth Conquers
+House Swyft - Awake! Awake!
+House Tallhart - Proud and Free
+House Targaryen - Fire and Blood
+House Tarly - First in Battle
+House Tollett - When All is Darkest
+House Toyne - Fly High, Fly Far
+House Trant - So End Our Foes
+House Tully - Family, Duty, Honor
+House Tyrell - Growing Strong
+House Velaryon - The Old, the True, the Brave
+House Waxley - Light in Darkness
+House Wendwater - For All Seasons
+House Wensington - Sound the Charge
+House Westerling - Honor, not Honors
+House Wode - Touch Me Not
+House Wydman - Right Conquers Might
+House Yronwood - We Guard the Way"""
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    words_map = dict()
+
+    for line in words_raw.split("\n"):
+        if line:
+            house, words = line.split(" - ")
+            _, house = house.split(None, 1)
+            words_map[house] = words.strip()
+
+    print("List of houses")
+    for house in words_map.keys():
+        print(house)
+
+    print("Python lookup dict")
+    pprint.pprint({k.lower(): v for k, v in words_map.items()})
+
+if __name__ == "__main__":
+    sys.exit(main())
